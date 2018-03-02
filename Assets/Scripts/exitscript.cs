c8d873e2d4da76a85f7dd1f598554bfcbf325e4b
Shuker <Shuker@Shukers-iMac.local>
2018-03-02 18:47:43 -0500

    Initial commit

---
+++ Assets/Scripts/exitscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exitscript : MonoBehaviour {
+	public BoxCollider2D self;
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (self.IsTouchingLayers(LayerMask.GetMask("Default"))) {
+			SceneManager.LoadScene ("WinScene");
+		}
+	}
+}
---
+++ Assets/Scripts/playercontrol.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playercontrol : MonoBehaviour {
+	public float move;
+	public float jump;
+	public Rigidbody2D pl;
+
+	// Use this for initialization
+	void Start () {
+		pl = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//jump
+		if (Input.GetKey(KeyCode.Space) && pl.IsTouchingLayers(LayerMask.GetMask("platforms"))) {
+			pl.AddForce (transform.up * jump, ForceMode2D.Impulse);
+		}
+
+		//move
+		if (Input.GetKey (KeyCode.D) ) {
+			pl.AddForce (transform.right * move, ForceMode2D.Impulse);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			pl.AddForce (-transform.right * move, ForceMode2D.Impulse);
+		}
+
+		//detections
+		if (pl.IsTouchingLayers(LayerMask.GetMask("killers"))) {
+			SceneManager.LoadScene("SimplePlat");
+		}
+
+	}
+}
